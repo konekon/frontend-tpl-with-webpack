c18428c635289ef9114774afb28a8f42a97cd118
igorjan94 <igorjan94@mail.ru>
2016-11-18 10:38:06 +0000

    LOLLOLLOL

--- sources/app/components/pages/djopa.js
+++ sources/app/components/pages/djopa.js
@@ -2,9 +2,12 @@ angular.module("app").config(($stateProvider, $locationProvider) => {
     $stateProvider.state({
         name: 'djopa',
         url: "/ttt",
-        template: require("./djopa500.html")
     });
     $locationProvider.html5Mode(true).hashPrefix("!");
-    console.log("hui");
+}).component('djopa', {
+    template: require("./djopa500.html"),
+    controller: function(socketService) {
+        this.socketService = socketService;
+    }
 });
 
